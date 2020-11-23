5f76fee2eb5af8f4e1e3511efb551cdfc0a2c0e6
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2020-11-23 17:15:49 +0200

    Adapt to OJS 3.2

--- SocialMetaPlugin.inc.php
+++ SocialMetaPlugin.inc.php
@@ -23,8 +23,8 @@ class SocialMetaPlugin extends GenericPlugin {
 		return __('plugins.generic.socialMeta.description');
 	}
 
-	function register($category, $path) {
-		if (parent::register($category, $path)) {
+	function register($category, $path, $mainContextId = null) {
+		if (parent::register($category, $path, $mainContextId)) {
 			if ($this->getEnabled()) {
 				HookRegistry::register('ArticleHandler::view',array(&$this, 'articleMeta'));
 			}
@@ -34,28 +34,39 @@ class SocialMetaPlugin extends GenericPlugin {
 	}
 
 	function articleMeta($hookName, $args) {
+		/**
+		 * @var $article Submission
+		 * @var $publication Publication
+		 * @var $journal Journal
+		 */
 		$request = $args[0];
 		$article = $args[2];
+		$publication = $args[3];
 		$journal = $request->getContext();
-		$locale = $journal->getPrimaryLocale();
 
 		$templateMgr = TemplateManager::getManager($request);
 
-		$templateMgr->addHeader('ogURL', '<meta property="og:url" content="' . $request->url(null, 'article', 'view', array($article->getBestArticleId())) . '"/>');
+		$publicationUrl = $publication->getId() !== $article->getCurrentPublication()->getId() ?
+			$request->url(null, 'article', 'view', array('version', $publication->getId())) :
+			$request->url(null, 'article', 'view', array($article->getId()));
+		$templateMgr->addHeader('ogURL', '<meta property="og:url" content="' . $publicationUrl . '"/>');
 
 		$templateMgr->addHeader('ogType', '<meta property="og:type" content="article"/>');
 
-		$templateMgr->addHeader('ogTitle', '<meta property="og:title" content="' . trim(htmlspecialchars($article->getLocalizedTitle())) . '"/>');
-		$templateMgr->addHeader("twitterTitle", '<meta name="twitter:title" content="' . trim(htmlspecialchars($article->getLocalizedTitle())) . '"/>');
+		$title = $publication->getLocalizedTitle();
+		$templateMgr->addHeader('ogTitle', '<meta property="og:title" content="' . trim(htmlspecialchars($title)) . '"/>');
+		$templateMgr->addHeader("twitterTitle", '<meta name="twitter:title" content="' . trim(htmlspecialchars($title)) . '"/>');
 
-		if ($article->getAbstract($locale)) {
-			$templateMgr->addHeader('ogDescription', '<meta property="og:description" content="' . htmlspecialchars(strip_tags($article->getAbstract($locale))) . '"/>');
-			$templateMgr->addHeader('twitterDescription', '<meta name="twitter:description" content="' . htmlspecialchars(strip_tags($article->getAbstract($locale))) . '"/>');
+		$abstract = $publication->getLocalizedData('abstract');
+		if ($abstract) {
+			$templateMgr->addHeader('ogDescription', '<meta property="og:description" content="' . htmlspecialchars(strip_tags($abstract)) . '"/>');
+			$templateMgr->addHeader('twitterDescription', '<meta name="twitter:description" content="' . htmlspecialchars(strip_tags($abstract)) . '"/>');
 		}
 
-		if ($article->getLocalizedCoverImageUrl()) {
-			$templateMgr->addHeader('ogImage', '<meta property="og:image" content="' . $article->getLocalizedCoverImageUrl() . '"/>');
-			$templateMgr->addHeader('twitterImage', '<meta name="twitter:image" content="' . $article->getLocalizedCoverImageUrl() . '"/>');
+		$imageUrl = $publication->getLocalizedCoverImageUrl($journal->getId());
+		if (!empty($imageUrl)) {
+			$templateMgr->addHeader('ogImage', '<meta property="og:image" content="' . $imageUrl . '"/>');
+			$templateMgr->addHeader('twitterImage', '<meta name="twitter:image" content="' . $imageUrl . '"/>');
 		}
 
 		$templateMgr->addHeader("twitterCard", '<meta name="twitter:card" content="summary_large_image"/>');
